6c4ece91b225f65f888cb7ee7cf5f16153d8f89f
Woosuk Suh <pipoket88@gmail.com>
2017-02-13 14:55:36 +0900

    Make py2exe to work under updated codebase and Python3.x environment

--- src/setup.py
+++ src/setup.py
@@ -10,24 +10,24 @@ from cx_Freeze import setup, Executable
 
 
 if sys.platform != "win32":
-    print "This script is only for Win32 build as of now!"
+    print("This script is only for Win32 build as of now!")
     sys.exit(1)
 
 base = "Win32GUI"
 
 setup(
         name = "booru-grabber",
-        version = "0.2.2",
+        version = "0.2.4",
         description = "Booru Grabber",
         options = {
             "build_exe": {
                 "includes": ["os"],
                 "excludes": ["doctest", "pdb", "unittest", "difflib",
-                    "optparse", "_gtkagg", "_tkagg",
-                    "bsddb", "curses", "email", "pywin.debugger", "tcl"],
-                "compressed": True,
-                "optimize": 1,
-                "create_shared_zip": True,
+                    "optparse", "_gtkagg", "_tkagg", "Tkconstants", "Tkinter",
+                    "bsddb", "curses", "pywin.debugger", "tcl"],
+                "optimize": 2,
+                "zip_include_packages": "*",
+                "zip_exclude_packages": None,
                 "include_msvcr": True,
             },
         },
@@ -36,12 +36,25 @@ setup(
 
 
 # Compress the files with UPX
-output_path = os.path.join("build", "exe.win32-2.7")
+output_path = os.path.join("build", "exe.win32-3.6")
 upx_base_args = ["upx", '--best', '--no-progress']
+
+# FIXME: These DLLs MUST NOT be compressed for Grabber to run properly.
+#        It is not known why UPX compression causes such error.
+upx_exclude_compress = [
+        "vcruntime140.dll",
+        "wxbase30u_net_vc140.dll",
+        "wxbase30u_vc140.dll",
+        "wxmsw30u_core_vc140.dll",
+]
+
 
 for filename in os.listdir(output_path):
     if (filename == "grabber.exe" or
             os.path.splitext(filename)[1].lower() in ('.exe','.dll','.pyd', '.so')):
+        if filename.lower() in upx_exclude_compress:
+            print("Skip UPX compression of {}".format(filename))
+            continue
         filepath = os.path.join(output_path, filename)
         args = ["upx", "--best", filepath]
         subprocess.call(args)
